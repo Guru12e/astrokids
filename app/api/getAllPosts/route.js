e9063de5f23156bfae4f034d3e051fa3bfee7d4c
Guru12e <guruvijaym21ec@psnacet.edu.in>
2025-04-05 15:36:05 +0530

    Update

--- app/api/getAllPosts/route.js
+++ app/api/getAllPosts/route.js
@@ -9,7 +9,7 @@ export async function GET() {
     await client.connect();
     const database = client.db("AstroKids");
     const collection = database.collection("blogs");
-    const blogs = (await collection.find(query).toArray()).reverse();
+    const blogs = (await collection.find().toArray()).reverse();
 
     return NextResponse.json(blogs, { status: 200 });
   } catch (error) {
