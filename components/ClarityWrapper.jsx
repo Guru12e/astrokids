059b9afa228a70cdabf6362b02e752784555cb0d
Guru12e <guruvijaym21ec@psnacet.edu.in>
2025-10-29 12:12:18 +0530

    Update

--- components/ClarityWrapper.jsx
+++ components/ClarityWrapper.jsx
@@ -4,7 +4,7 @@ import Clarity from "@microsoft/clarity";
 
 export default function ClarityWrapper() {
   useEffect(() => {
-    Clarity.init("twnbfuotyc");
+    Clarity.init("sdlo5wowr6");
   }, []);
 
   return null;
